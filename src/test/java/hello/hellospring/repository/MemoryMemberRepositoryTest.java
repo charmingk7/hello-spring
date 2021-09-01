f381ef540f2750dbea969c0078a9b0efe1a203fe
developer <blue007m@naver.com>
2021-09-01 21:30:20 +0900

    repository 테스트

---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @Test
+    public void save() {
+        //given
+        Member member = new Member();
+        member.setName("spring");
+
+        //when
+        repository.save(member);
+
+        //then
+        Member result = repository.findById(member.getId()).get();
+        assertThat(result).isEqualTo(member);
+    }
+
+}
