42e5fc2ff3dc937d5fd14418a07916fba07568ff
developer <blue007m@naver.com>
2021-09-01 22:03:11 +0900

    MemberService  테스트

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -5,6 +5,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,6 +31,42 @@ class MemoryMemberRepositoryTest {
         //then
         Member result = repository.findById(member.getId()).get();
         assertThat(result).isEqualTo(member);
+    }
+
+    @Test
+    public void findByName() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        //when
+        Member result = repository.findByName("spring1").get();
+
+        //then
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        //when
+        List<Member> result = repository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
     }
 
 }
